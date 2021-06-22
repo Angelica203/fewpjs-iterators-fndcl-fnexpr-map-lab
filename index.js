62c036c52fb579bf35c2c9092727d31865441b14
github username <github email address>
2021-06-22 19:24:44 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const titleCased = (input) => {
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  })
 }
+
+// tutorials.charAt(0).toUpperCase() + tutorials.slice(1) );
